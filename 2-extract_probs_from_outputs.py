acd58ff887dfddf75a1c50eb0ffa5305f86a1a64
c0j0s <c0j0s@outlook.com>
2023-10-30 00:26:34 +0800

    add read pcsp outputs

---
+++ 2-extract_probs_from_outputs.py
@@ -0,0 +1,56 @@
+import os
+import re
+import csv
+
+path = "./outputs"
+pattern = r"(?<=\[).*(?=\])"
+mode = 'avg'
+output = "./data"
+
+def read_directory(path, extension='.txt'):
+    return [f for f in os.listdir(path) if f.endswith(extension)]
+
+def write_csv(file_path, dataset):
+    with open(file_path, 'w', newline='') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=['match_id', 'prob'])
+        writer.writeheader()
+        writer.writerows(dataset)
+
+def main():
+    files = read_directory(path)
+    print(f'Found {len(files)} txt files in {path}, extracting probabilities...')
+
+    away_dataset = []
+    home_dataset = []
+    for file in files:
+        with open(f'{path}/{file}', 'r') as f:
+            meta = file.replace(".txt", "").split("_")
+            print(f'probs of match:{meta[0]} for team {meta[1]}:')
+            text = f.read()
+            matches = re.findall(pattern, text)
+            probs_raw = [p.split(",") for p in matches]
+            probs = []
+            for rates in probs_raw:
+                prob = {
+                    'min': float(rates[0]),
+                    'max': float(rates[1]),
+                    'avg': (float(rates[0]) + float(rates[1])) / 2.0
+                }
+                probs.append(prob)
+            print(f'{meta[1]} -> {probs}')
+            if meta[1] == "home":
+                home_dataset.append({
+                    "match_id"  :meta[0],
+                    "prob"      :probs[0][mode]
+                })
+            else:
+                away_dataset.append({
+                    "match_id"  :meta[0],
+                    "prob"      :probs[0][mode]
+                })
+            
+    write_csv(f'{output}/away_probs.csv', away_dataset)
+    write_csv(f'{output}/home_probs.csv', home_dataset)
+
+if __name__ == '__main__':
+    main()
---
+++ 3-compute_softmax.py
@@ -0,0 +1,37 @@
+# %%
+import pandas as pd
+import numpy as np
+
+away_input='data/away_probs.csv'
+home_input='data/home_probs.csv'
+output='probabilities/'
+
+# %%
+df_away = pd.read_csv(away_input)
+df_away = df_away.rename(columns={'prob': 'prob_away'})
+df_home = pd.read_csv(home_input)
+df_home = df_home.rename(columns={'prob': 'prob_home'})
+
+# %%
+def softmax(column):
+    e_x = np.exp(column - np.max(column))  # Subtracting the max value for numerical stability
+    return e_x / e_x.sum()
+
+def write_csv(path, dataset):
+    dataset.to_csv(path, index=False)
+
+# %%
+merged_df = pd.merge(df_away, df_home, on='match_id', how='inner')
+
+# %%
+softmax_df = merged_df.copy()
+
+# %%
+softmax_df['match_url'] = merged_df['match_id'].apply(lambda x: f'https://www.premierleague.com/match/12301/{x}')
+softmax_df[['away_prob_softmax', 'home_prob_softmax']] = merged_df[['prob_away', 'prob_home']].apply(softmax, axis=1)
+
+# %%
+write_csv(f'{output}/away.csv', softmax_df[['match_url', 'away_prob_softmax']])
+write_csv(f'{output}/home.csv', softmax_df[['match_url', 'home_prob_softmax']])
+
+
